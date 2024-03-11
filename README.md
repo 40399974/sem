7056c4bef4c08f88ca7bd08325e908f817e90e3d
JackRodger18 <40628388@live.napier.ac.uk>
2024-03-11 21:30:56 +0000

    connect to mysql db

--- README.md
+++
@@ -1,9 +0,0 @@
-# Software Engineering Methods
-
-* Master Build Status ![GitHub Workflow Status (branch)](https://img.shields.io/github/actions/workflow/status/40399974/sem/main.yml?branch=master) 
-
-* Develop Branch Status [![LICENSE](https://img.shields.io/github/license/40399974/sem.svg?style=flat-square)](https://github.com/40399974/sem/blob/main/LICENSE) 
-
-* License [![Releases](https://img.shields.io/github/release/40399974/sem/all.svg?style=flat-square)](https://github.com/40399974/sem/releases)
-
-* Release ![GitHub Workflow Status (branch)](https://img.shields.io/github/actions/workflow/status/40399974/sem/developer.yml?branch=developer)
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,36 @@
 package com.napier.sem;
 
+import java.sql.*;
+
 public class App
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Boo yah!");
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/world";
+        String username = "root";
+        String password = "root_password";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected to the database!");
+
+            // Example query: Retrieve names and populations of cities
+            String query = "SELECT name, population FROM country ORDER BY population DESC";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(query)) {
+
+                // Process the result set
+                while (resultSet.next()) {
+                    String cityName = resultSet.getString("Name");
+                    int population = resultSet.getInt("Population");
+
+                    System.out.println("City: " + cityName + ", Population: " + population);
+                }
+            }
+
+            // Now you can execute SQL queries
+            // Example: SELECT * FROM city
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
